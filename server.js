196758de49336a87d015b91fdc2320500181b871
= <=>
2021-11-06 15:16:37 -0600

    Initial Author Routes

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
+//Only load dotenv if we're not in production environment
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-console.log(process.env);
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
 //Default router
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index');
+const authorRouter = require("./routes/authors");
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
+app.set('layout', 'layouts/layout') //Every file will use this as an HTML template
 app.use(expressLayouts)
-app.use(express.static('public'))
+app.use(express.static('public')) //Style sheets, js files, images
+
+// Parses body to json
+app.use(express.json())
+app.use(express.urlencoded({ limit: "10mb", extended: true }))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
@@ -22,6 +27,8 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+//Route to default router
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
