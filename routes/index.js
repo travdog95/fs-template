e87db449968373fb1134ce4ae86dde955be68e7f
= <=>
2021-11-08 17:36:11 -0700

    Books create/index/new

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const router = express.Router()
-// const Book = require('../models/book')
+const Book = require('../models/book')
 
 router.get('/', async (req, res) => {
-  // let books
-  // try {
-  //   books = await Book.find().sort({ createdAt: 'desc' }).limit(10).exec()
-  // } catch {
-  //   books = []
-  // }
-  // res.render('index', { books: books })
-  res.render('index');
+  let books = [];
+  try {
+    books = await Book.find().sort({ createdAt: 'desc' }).limit(10).exec()
+  } catch {
+    books = []
+  }
+  res.render('index', { books: books })
 })
 
 module.exports = router
