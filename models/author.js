2a0d3470338b15dbafa321b2cd576aac447b8f69
= <=>
2021-11-12 10:00:43 -0700

    Finished author route

--- models/author.js
+++ models/author.js
@@ -1,8 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const Book = require("./book");
 
 const authorSchema = new mongoose.Schema({
-  name: { type: String, required: true}
-})
+  name: { type: String, required: true },
+});
+
+//Run this code before a remove method is called
+authorSchema.pre("remove", function (next) {
+  Book.find({ author: this.id }, (err, books) => {
+    if (err) {
+      //Can't find author
+      next(err);
+    } else if (books.length > 0) {
+      //Author has books
+      next(new Error("This author has books still"));
+    } else {
+      //No erros, remove author
+      next();
+    }
+  });
+});
 
 // Name of table in database
-module.exports = mongoose.model('Author', authorSchema);
+module.exports = mongoose.model("Author", authorSchema);
